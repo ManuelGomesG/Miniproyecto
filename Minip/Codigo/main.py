f1122b4f04fe204cc65f01559efe4a8131d3dea1
Manuel Gomes <manuelgomesg@gmail.com>
2017-11-12 17:41:25 -0400

    adaptado a búsqueda de instancia por clases

--- Minip/Codigo/main.py
+++ Minip/Codigo/main.py
@@ -9,18 +9,65 @@ from IPython.display import display
 import savefig as sf
 import sys
 import wfdbi
+import csv
 
 
 
+tc=0
+normalc=0
+noisyc=0
+ac=0
+oc=0
 
-fullpath=sys.argv[1][:-4]
-f=open(fullpath+".hea", "r")
-samp=int(f.readline().split(" ")[3])
+fullpath=sys.argv[1]
+rpath=fullpath+"REFERENCE-v3.csv"
+with open(rpath, 'rb') as csvfile:
+     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+     for row in spamreader:
+         rname  = ', '.join(row).split(",")[0]
+         rclass = ', '.join(row).split(",")[1]
 
-record = wfdb.rdsamp( fullpath , sampto=samp)
-rname=fullpath.split("/")[-1]
-print(rname)
-fig=wfdbi.plotrec(record, title=rname, timeunits='seconds',figsize = (200,5), ecggrids='all', returnfig = True)
+         print" tc: " , tc, "oc: ", oc, "noisy: ", noisyc, "ac: ", ac
+
+         if tc<430:
+
+             if rclass=="N":
+                if normalc<257:
+                    normalc+= 1
+                    tc+= 1
+                else:
+                    continue
+             elif rclass=="O":
+                if oc<128:
+                    oc+= 1
+                    tc+= 1
+                else:
+                    continue
+             elif rclass=="~":
+                if noisyc<5:
+                    noisyc+= 1
+                    tc+= 1
+                else:
+                    continue
+             elif rclass=="A":
+                if ac<40:
+                    ac+= 1
+                    tc+= 1
+                else:
+                    continue
+
+             f=open(fullpath+rname+".hea", "r")
+             samp=int(f.readline().split(" ")[3])
+             record = wfdb.rdsamp(  fullpath+rname, sampto=samp)
+
+
+             print(rname)
+             wfdbi.plotrec(record, title=rname, timeunits='seconds',figsize = (200,5), returnfig = False)
+         else:
+             quit()
+
+
+
 #fig.savefig("/home/manuel/Documents/Minip/"+rname+".png")
 #sf.save(rname,ext="png", close=True, verbose=True)
 
