0d27723f182c2f10ff5dce02e6eca9624e038db1
Manuel Gomes <manuelgomesg@gmail.com>
2017-11-14 14:53:31 -0400

    puesto como función

--- Minip/Codigo/plotting.py
+++ Minip/Codigo/plotting.py
@@ -5,19 +5,20 @@ import wfdbi
 import wfdb
 
 
+def plotting(fullpath='',grid=1,out=''):
+    pass
+    #fullpath=sys.argv[1]
+    sig=fullpath.split("/")[-1]
 
-fullpath=sys.argv[1]
-sig=fullpath.split("/")[-1]
+    #print sig
 
-#print sig
-
-f=open(fullpath+".hea", "r")
-samp=int(f.readline().split(" ")[3])
-record = wfdb.rdsamp(  fullpath, sampto=samp)
-#print(rname)
-if sys.argv[1]==1:
-    wfdbi.plotrec(record, title=sig, timeunits='seconds',figsize = (200,5),
-        ecggrids='all',returnfig = False, picpath=sys.argv[3])
-else:
-    wfdbi.plotrec(record, title=sig, timeunits='seconds',figsize = (200,5),
-        returnfig = False, picpath=sys.argv[3])
+    f=open(fullpath+".hea", "r")
+    samp=int(f.readline().split(" ")[3])
+    record = wfdb.rdsamp(  fullpath, sampto=samp)
+    #print(rname)
+    if grid==1:
+        wfdbi.plotrec(record, title=sig, timeunits='seconds',figsize = (200,5),
+            ecggrids='all',returnfig = False, picpath=out)
+    else:
+        wfdbi.plotrec(record, title=sig, timeunits='seconds',figsize = (200,5),
+            returnfig = False, picpath=out)
